efa5f93119a8408f74f00754cf7d7b4a627a5448
eslalaz <eslalazarus23@gmail.com>
2023-07-23 12:11:19 -0700

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+flask model
 """
-
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def hbnb():
+    """
+        / home path
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
+def index():
+    """
+        /hbnb path
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def C_is(text):
+    """
+        /C path
+    """
+    return 'C {:s}'.format(text.replace('_', ' '))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
